1b0858a5d8faff5b71aa31b2b58fdf02323c3e94
Raghvendra Dharmapurikar <raghvendra.d@gmail.com>
2019-07-08 08:02:48 +0530

    Added method for parsing job graph

--- assignment.rb
+++ assignment.rb
@@ -1,2 +1,18 @@
 class Assignment
+
+  def parse_input_jobs(job_as_string)
+    job_graph = Hash.new
+    jobs_with_dependency = job_as_string.split("\n").reject do |job|
+      job.nil? || job.strip.empty?
+    end
+    .map do |job|
+      job.strip
+    end
+    jobs_with_dependency.each do |job_with_dependency|
+      jobs = job_with_dependency.split("=>").map { |job| job.strip }
+      job_graph[jobs[0]] = jobs[1]
+    end
+    job_graph
+  end
+
 end
--- assignment_tests.rb
+++ assignment_tests.rb
@@ -7,4 +7,15 @@ class AssignmentTests < Minitest::Test
     @assignment = Assignment.new
   end
 
+  def test_parse_jobs
+    jobs = """
+    a => b
+    b => c
+    """
+    job_graph = @assignment.parse_input_jobs(jobs)
+    assert_equal "b", job_graph["a"]
+    assert_equal "c", job_graph["b"]
+    assert_equal 2, job_graph.size
+  end
+
 end
