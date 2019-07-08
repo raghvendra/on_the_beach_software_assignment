cd5c4f57bcae161ec44340fcd3b6af225a168afa
Raghvendra Dharmapurikar <raghvendra.d@gmail.com>
2019-07-08 08:52:06 +0530

    1. Added method for performing jobs in order of their dependency. 2. Updated the test cases

--- assignment.rb
+++ assignment.rb
@@ -43,4 +43,72 @@ class Assignment
     return false
   end
 
+  def check_if_job_depends_on_self(job_graph)
+    job_graph.keys.each do |job|
+      if job_graph[job] == job
+        return true
+      end
+    end
+    return false
+  end
+
+  def create_reverse_job_graph(job_graph)
+    reverse_graph = Hash.new
+    job_graph.each do |key,value|
+      if value.nil?
+        next
+      end
+      if reverse_graph.key? value
+        reverse_graph[value].append(key)
+      else
+        reverse_graph[value] = [key]
+      end
+    end
+    reverse_graph
+  end
+
+  def perform_jobs(job_description)
+    job_graph = parse_input_jobs(job_description)
+
+    is_any_job_depends_on_itself = check_if_job_depends_on_self(job_graph)
+    if is_any_job_depends_on_itself
+      puts "Can not perform jobs : A job depends on itself"
+      return
+    end
+
+    is_cycle_present = check_presence_of_cycle_in_job_graph(job_graph)
+    if is_cycle_present
+      puts "Can not perform jobs : presence of cycle detected"
+      return
+    end
+
+    # Get the difference between job_graph and reverse_job_graph
+    # This is to find out the jobs which can be performed independently
+    r_job_graph = create_reverse_job_graph(job_graph)
+    job_graph_keys = job_graph.select { |k,v| k unless v.nil? }.keys
+    independent_jobs = r_job_graph.keys - job_graph_keys
+
+    jobs_to_perform = independent_jobs
+    performed_jobs = Array.new
+
+    # Loop through the jobs_to_perform
+    # Perform the job and add the job which are dependent on it
+    while jobs_to_perform.length != 0
+      job = jobs_to_perform.shift
+      if job.nil?
+        next
+      end
+      unless performed_jobs.include? job
+        performed_jobs.append(job)
+      end
+      if r_job_graph.key? job
+        dependent_jobs = r_job_graph[job]
+        dependent_jobs.each do |j|
+          jobs_to_perform.append(j)
+        end
+      end
+    end
+    performed_jobs
+  end
+
 end
--- assignment_tests.rb
+++ assignment_tests.rb
@@ -7,7 +7,7 @@ class AssignmentTests < Minitest::Test
     @assignment = Assignment.new
   end
 
-  def test_parse_jobs
+  def test_job_description_can_be_parsed
     jobs = """
     a => b
     b => c
@@ -18,17 +18,17 @@ class AssignmentTests < Minitest::Test
     assert_equal 2, job_graph.size
   end
 
-  def test_job_graph_with_cycle1
+  def test_presence_of_self_dependent_job_is_detected
     jobs = """
     a => a
     b => c
     """
     job_graph = @assignment.parse_input_jobs(jobs)
-    is_cycle_present = @assignment.check_presence_of_cycle_in_job_graph(job_graph)
+    is_cycle_present = @assignment.check_if_job_depends_on_self(job_graph)
     assert_equal true, is_cycle_present
   end
 
-  def test_job_graph_with_cycle1
+  def test_presence_of_cycle_is_detected
     jobs = """
     a => b
     b => c
@@ -37,5 +37,61 @@ class AssignmentTests < Minitest::Test
     job_graph = @assignment.parse_input_jobs(jobs)
     is_cycle_present = @assignment.check_presence_of_cycle_in_job_graph(job_graph)
     assert_equal true, is_cycle_present
+  end
+
+  def test_perform_jobs_return_nil_if_cycle_is_present
+    jobs = """
+    a => b
+    b => c
+    c => a
+    """
+    performed_jobs = @assignment.perform_jobs(jobs)
+    assert_nil performed_jobs
+  end
+
+  def test_perform_jobs_return_nil_if_job_depends_on_self
+    jobs = """
+    a => b
+    b => c
+    c => c
+    """
+    performed_jobs = @assignment.perform_jobs(jobs)
+    assert_nil performed_jobs
+  end
+
+  def test_job_graph_case_1
+    jobs = """
+    a => b
+    b => c
+    c => d
+    d =>
+    """
+    performed_jobs = @assignment.perform_jobs(jobs)
+    assert_equal ["d", "c", "b", "a"], performed_jobs
+  end
+
+  def test_job_graph_case_2
+    jobs = """
+    a =>
+    b => c
+    c => f
+    d => a
+    e => b
+    f =>
+    """
+    performed_jobs = @assignment.perform_jobs(jobs)
+    assert_equal ["f", "a", "c", "d", "b", "e"], performed_jobs
+  end
+
+  def test_job_graph_case_3
+    job_description = """
+    a => b
+    a => c
+    b => d
+    c => d
+    d =>
+    """
+    performed_jobs = @assignment.perform_jobs(job_description)
+    assert_equal ["d", "b", "c", "a"], performed_jobs
   end
 end
