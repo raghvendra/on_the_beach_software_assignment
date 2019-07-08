27d492ef74d682409deca671a25e681de2135caf
Raghvendra Dharmapurikar <raghvendra.d@gmail.com>
2019-07-08 08:13:17 +0530

    Added methods for checking presence of a cycle in a job graph

--- assignment.rb
+++ assignment.rb
@@ -1,8 +1,8 @@
 class Assignment
 
-  def parse_input_jobs(job_as_string)
+  def parse_input_jobs(jobs_as_string)
     job_graph = Hash.new
-    jobs_with_dependency = job_as_string.split("\n").reject do |job|
+    jobs_with_dependency = jobs_as_string.split("\n").reject do |job|
       job.nil? || job.strip.empty?
     end
     .map do |job|
@@ -13,6 +13,34 @@ class Assignment
       job_graph[jobs[0]] = jobs[1]
     end
     job_graph
+  end
+
+  def check_presence_of_cycle_from_job(starting_job, job_graph)
+    job_stack = [starting_job]
+    visited_jobs = [starting_job]
+
+    while job_stack.length != 0
+      job = job_stack.pop
+      if job_graph.key?(job)
+        dependent_job = job_graph[job]
+        if visited_jobs.include? dependent_job
+          return true
+        else
+          visited_jobs.append(dependent_job)
+          job_stack.append(dependent_job)
+        end
+      end
+    end
+    false
+  end
+
+  def check_presence_of_cycle_in_job_graph(job_graph)
+    job_graph.keys.each do |job|
+      if check_presence_of_cycle_from_job(job, job_graph)
+        return true
+      end
+    end
+    return false
   end
 
 end
--- assignment_tests.rb
+++ assignment_tests.rb
@@ -18,4 +18,24 @@ class AssignmentTests < Minitest::Test
     assert_equal 2, job_graph.size
   end
 
+  def test_job_graph_with_cycle1
+    jobs = """
+    a => a
+    b => c
+    """
+    job_graph = @assignment.parse_input_jobs(jobs)
+    is_cycle_present = @assignment.check_presence_of_cycle_in_job_graph(job_graph)
+    assert_equal true, is_cycle_present
+  end
+
+  def test_job_graph_with_cycle1
+    jobs = """
+    a => b
+    b => c
+    c => a
+    """
+    job_graph = @assignment.parse_input_jobs(jobs)
+    is_cycle_present = @assignment.check_presence_of_cycle_in_job_graph(job_graph)
+    assert_equal true, is_cycle_present
+  end
 end
